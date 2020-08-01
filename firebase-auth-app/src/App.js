1e86925ba81086ec5c65492c5bde97f5c55e844b
sojin-hwang <qrt56@naver.com>
2020-08-02 03:33:29 +0900

    calendar

--- firebase-auth-app/src/App.js
+++ firebase-auth-app/src/App.js
@@ -6,10 +6,15 @@ import Application from "./components/Application";
 import UserProvider from "./providers/UserProvider";
 import ProfilePage from "./components/ProfilePage";
 import { UserContext } from "./providers/UserProvider";
+import Calendar from 'react-calendar';
+import calendar from './components/Calendar/Calendar'
+import CalendarView from './components/CalendarView'
+
 function App() {
   return (
     <UserProvider>
       <Application />
+      <CalendarView/>
     </UserProvider>
   );
 }
